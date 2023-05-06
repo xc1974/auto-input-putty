d1ddcae09032a8c7d523f05de388bf95cdecf274
xc1974 <105591966+xc1974@users.noreply.github.com>
2023-05-06 16:12:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,62 +1,18 @@
-@@ -0,0 +1,61 @@
-import os
-import requests
-import time
+import subprocess
+import os.path
 
-# URL of the GitHub file
-github_url = 'https://raw.githubusercontent.com/username/repo_name/main/file_name.py'
-
-# Local file path
-local_path = 'file_name.py'
-
-if os.path.isfile(local_path):
-    # Get the modification time of the local file
-    local_mod_time = os.path.getmtime(local_path)
-
-    # Get the content of the GitHub file
-    response = requests.get(github_url)
-    github_code = response.content.decode('utf-8')
-
-    # Get the modification time of the GitHub file
-    github_mod_time = time.mktime(time.strptime(response.headers['Last-Modified'], '%a, %d %b %Y %H:%M:%S %Z'))
-
-    # Check if the content of the local file is the same as the content of the GitHub file
-    with open(local_path, 'r') as f:
-        local_code = f.read()
-
-    if local_code != github_code:
-        # Calculate the time difference in seconds
-        time_diff = time.time() - local_mod_time
-
-        # Print the time difference in seconds
-        print(f'The local file is different from the GitHub file. Time difference: {time_diff} seconds.')
-
-        # Write the content of the GitHub file to the local file
-        with open(local_path, 'w') as f:
-            f.write(github_code)
-
-    # Run the updated code
-    exec(github_code)
+# Check if the ip_address.txt file exists
+if os.path.isfile("ip_address.txt"):
+    with open("ip_address.txt", "r") as f:
+        ip_address = f.read().strip()
 else:
-    # Get the content of the GitHub file
-    response = requests.get(github_url)
-    github_code = response.content.decode('utf-8')
+    # Ask user for the IP address if the file does not exist
+    ip_address = input("Please enter the IP address: ")
 
-    # Write the content of the GitHub file to the local file
-    with open(local_path, 'w') as f:
-        f.write(github_code)
+# Run the command with the given IP address
+command = f"putty.exe -ssh {ip_address} -l pi -pw lkkcict306"
+subprocess.run(command, shell=True)
 
-    # Run the code
-    exec(github_code)
-
-# Check if input file exists
-if os.path.isfile('input.txt'):
-    # Read input value from file
-    with open('input.txt', 'r') as f:
-        ipaddress = f.readline().strip()
-else:
-    # Ask user for input value
-    ipaddress = input("Please enter the IO address: ")
-
-# Run Putty.exe with SSH address, username, and password
-os.system(f'putty.exe -ssh {ipaddress} -l pi -pw lkkcict306')
+# Save the IP address into a file for future use
+with open("ip_address.txt", "w") as f:
+    f.write(ip_address)
