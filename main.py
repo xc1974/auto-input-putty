71e2bd0c8fb8211572de543d7ef5311135cc7a75
xc1974 <105591966+xc1974@users.noreply.github.com>
2023-05-15 20:26:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import subprocess
 import os.path
 
 # Check if the ip_address.txt file exists
-if os.path.isfile("ip_address.autoinput"):
-    with open("ip_address.autoinput", "r") as f:
+if os.path.isfile("ip_address.youcantopenithehe"):
+    with open("ip_address.youcantopenithehe", "r") as f:
         ip_address = f.read().strip()
 else:
     # Ask user for the IP address if the file does not exist
-    print("test:"+ip_address)
     ip_address = input("Please enter the IP address: ")
 
 # Run the command with the given IP address
+print("test:"+ip_address)
 command = f"putty.exe -ssh {ip_address} -l pi -pw lkkcict306"
 subprocess.run(command, shell=True)
 
 # Save the IP address into a file for future use
-with open("ip_address.autoinput", "w") as f:
+with open("ip_address.youcantopenithehe", "w") as f:
     f.write(ip_address)
