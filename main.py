cddda95937f08bb67a4c087a2e3147f45bb50459
xc1974 <105591966+xc1974@users.noreply.github.com>
2023-05-15 21:11:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import subprocess
 import os.path
+import time
 
 # Check if the ip_address.txt file exists
 if os.path.isfile("ip_address.youcantopenithehe"):
@@ -10,10 +11,12 @@ else:
     ip_address = input("Please enter the IP address: ")
 
 # Run the command with the given IP address
-print("test:"+ip_address)
-command = f"putty.exe -ssh {ip_address} -l pi -pw lkkcict306"
+command = f"putty.exe -ssh 192.158.1.{ip_address} -l pi -pw lkkcict306"
 subprocess.run(command, shell=True)
+print("debug:"+ip_address)
+print("if you see this message, you need to redownload it or you should contact me!")
 
 # Save the IP address into a file for future use
 with open("ip_address.youcantopenithehe", "w") as f:
     f.write(ip_address)
+time.sleep(1)
